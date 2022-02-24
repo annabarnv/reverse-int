ecd14f7ea0e10d7e765dc1c7e43813e4d91dadc0
Anna Baranova <annnanya163@gmail.com>
2022-02-24 15:44:27 +0300

    feat: add task solution

--- postinstall.js
+++ postinstall.js
@@ -6,7 +6,7 @@ const { engines: { node: nodeVersion }} = require('./package');
 if (!semver.satisfies(process.version, nodeVersion)) {
   process.emitWarning(
     colors.red(`
-      For this task we are strictly recomend you to use node ${nodeVersion}.
+      For this task we are strictly recommend you to use node ${nodeVersion}.
       Now you are using node ${process.version}, if you are using any of features that not supported by node ${nodeVersion}, score won't be submitted
     `)
   );
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  let numArr = n.toString().split('');
+  let revNum = '';
+  for(let i = 0; i < numArr.length; i++) {
+      revNum = numArr[i]+revNum ;
+  }
+  return revNum;
 }
