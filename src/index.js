7aa81b5407beeb19e78da2ab08dbffedf79c41fc
Anna Baranova <annnanya163@gmail.com>
2022-02-24 15:49:12 +0300

    refactor:write methods in one line

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function reverse (n) {
-  n = Math.abs(n);
-  let numArr = n.toString().split('');
+
+  let numArr = Math.abs(n).toString().split('');
   let revNum = '';
   for(let i = 0; i < numArr.length; i++) {
       revNum = numArr[i]+revNum ;
